01ca4db79b533085b396beff49ab23af09999c5e
pratikpatwe <pratikpatwe79@gmail.com>
2025-02-09 14:50:46 +0530

    error middleware implemented

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 import express from 'express';
 import { PORT } from './config/env.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/users.routes.js';
 import subRouter from './routes/subscription.routes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
   res.send('<p>Welcome to the <b>RecuroAPI</b> - Effortless management of recurring subscriptions and payments.</p><br><p>This is an API service.</p>');
