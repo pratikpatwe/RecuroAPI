801cd8917baa9e87a0e00dcc6d8511b1a050a2b5
pratikpatwe <pratikpatwe79@gmail.com>
2025-02-09 13:18:51 +0530

    created user data model

---
+++ models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from 'momngoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String, 
+    required: [true, 'Name is required'],
+    trim: true,
+    minLenght: [2, 'Name must be at least 2 characters long'],
+    maxLength: [50, 'Name must be at most 50 characters long'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    trim: true,
+    unique: true,
+    minLenght: [9, 'Email must be at least 9 characters long'],
+    maxLength: [50, 'Email must be at most 50 characters long'],
+    lowercase: true,
+    match: [
+      /\S+@\S+\.\S+/,
+      'Email must be a valid email address',
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minLenght: [8, 'Password must be at least 8 characters long'],
+    maxLength: [50, 'Password must be at most 50 characters long'],
+  },
+}, {timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
