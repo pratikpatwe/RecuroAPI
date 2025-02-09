799819e916030c77a94cbdad1fdc4c02fbc97cb2
pratikpatwe <pratikpatwe79@gmail.com>
2025-02-09 13:03:33 +0530

    connected mongodb

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { PORT } from './config/env.js';
+import connectToDatabase from './database/mongodb.js';
 
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/users.routes.js';
@@ -15,6 +16,8 @@ app.get('/', (req, res) => {
   res.send('<p>Welcome to the <b>RecuroAPI</b> - Effortless management of recurring subscriptions and payments.</p><br><p>This is an API service.</p>');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
 console.log(`Server is running on http://localhost:${PORT}`);
+
+await connectToDatabase();
 });
